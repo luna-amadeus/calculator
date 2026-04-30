7a9c96c5c639562d192a4356bf587271c80623c4
Luna Amadeus <leo.amadeus.scott@gmail.com>
2026-04-30 12:43:55 +1000

    Add basic arithmetic functions

---
+++ main.js
@@ -0,0 +1,17 @@
+// Basic arithmetic functions
+
+const add = (a, b) => {
+    return a + b;
+}
+
+const subtract = (a, b) => {
+    return a - b;
+}
+
+const multiply = (a, b) => {
+    return a * b;
+}
+
+const divide = (a, b) => {
+    return a / b;
+}
