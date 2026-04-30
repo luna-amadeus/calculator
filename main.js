cd1a6163c8768031fe475e71d7c7bd78f53fc6fc
Luna Amadeus <leo.amadeus.scott@gmail.com>
2026-04-30 13:34:36 +1000

    Skeleton Functions and changeCurrentOperator()

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const btns = document.querySelectorAll("button");
 const display = document.querySelector("#display");
+const output = document.querySelector("#output");
 
 
 
@@ -12,32 +13,75 @@ const display = document.querySelector("#display");
 
 const add = (a, b) => {
     return a + b;
-}
+};
 
 const subtract = (a, b) => {
     return a - b;
-}
+};
 
 const multiply = (a, b) => {
     return a * b;
-}
+};
 
 const divide = (a, b) => {
     return a / b;
-}
+};
 
 let firstNum;
 let secondNum;
 let operator;
+let currentNum = "firstNum";
 
 const operate = () => {
     return operator(firstNum, secondNum);
-}
+};
 
+const updateCurrentNum = (btn) => {
+    firstNum = btn;
+    secondNum = btn;
+    console.log(firstNum);
+};
+
+const updateCurrentOperator = (btn) => {
+    switch (btn) {
+        case ("add") :
+            operator = add;
+            break;
+        case ("subtract") :
+            operator = subtract;
+            break;
+        case ("multiply") :
+            operator = multiply;
+            break;
+        case ("divide") :
+            operator = divide;
+            break;
+    }
+};
+
+const decimal = () => {
+    firstNum += ".";
+};
+
+const completeEquation = () => {
+    operate();
+};
+
+const clearCalc = () => {
+    console.log("The calculator has been cleared");
+};
 
 const clickBtn = (e) => {
-    console.log(e.target.id);
-}
+    const btnPressed = e.target.id;
+    switch (btnPressed) {
+        case "add" :
+        case "subtract" :
+        case "multiply" :
+        case "divide" :
+            updateCurrentOperator(btnPressed);
+            break;
+    }
+};
 
 
 
